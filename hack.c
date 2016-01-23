e2f22853a708ad5993a3917f5efcf61d583c07a7
Lambert Wang <lambert.ww@gmail.com>
2016-01-22 22:15:57 -0500

    gitignore and version core

--- hack.c
+++ hack.c
@@ -9,14 +9,25 @@
 int main(void){
 	int width = 1280;
 	int height = 720;
+
+
 	GLFWwindow * window;
 	if(!glfwInit()) return -1;
+	
+	// Set window hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Generate window and set context
 	window = glfwCreateWindow(width, height, "hack", NULL, NULL);
 	if(!window){
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
 	GLenum glewError = glewInit();
 	if(glewError != GLEW_OK){
 		printf("Error initializing GLEW!%s\n", glewGetErrorString(glewError));
