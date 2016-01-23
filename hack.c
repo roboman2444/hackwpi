3d64ab20086f6f47e10ab87b6241776144552bd5
roboman2444 <roboman2444@gmail.com>
2016-01-22 22:07:44 -0500

    glew and shit is init

--- hack.c
+++ hack.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glfw3.h>
 int main(void){
+	GLFWwindow * window;
+	if(!glfwInit()) return -1;
+	window = glfwCreateWindow(width, height, "hack", NULL, NULL);
+	if(!window){
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	GLenum glewError = glewInit();
+	if(glewError != GLEW_OK){
+		printf("Error initializing GLEW!%s\n", glewGetErrorString(glewError));
+		return FALSE;
+	}
+	//gl is all set up
+	return 0;
 } //todo
